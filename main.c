a43b8fcde267ef36c431823a09fdada2edb030b0
Neil Dick <neildick@microsoft.com>
2020-02-04 08:50:54 +1100

    Hold reset during tetris

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "alta_gpio.h"
+
 int
 main(void)
 {
+    set_gpio_direction(GPIO_PIN6_VAL, GPIO_OUTPUT);
+
+    set_gpio(GPIO_PIN6_OFFSET, GPIO_PIN6_VAL);
+    clear_gpio(GPIO_PIN6_OFFSET);
+    set_gpio(GPIO_PIN6_OFFSET, GPIO_PIN6_VAL);
+
     tetris_run(GAME_WIDTH, GAME_HEIGHT);
     return EXIT_SUCCESS;
 }
